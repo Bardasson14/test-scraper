675b69eac4861dd4ddaca2f665ffcb970353c397
laaapf <lucaspfonseca98@gmail.com>
2022-10-26 23:46:02 -0300

    ajustando saida do script

--- test-scraper-git/main.py
+++ test-scraper-git/main.py
@@ -7,6 +7,8 @@ load_dotenv(find_dotenv())
 
 #github.enable_console_debug_logging()
 if __name__ == "__main__":
+    json_dict = {}
+    json_dict["config_file"]=None
     g = github.Github(os.environ.get("GITHUB_TOKEN"))
     build_lib = None
     if len(sys.argv) == 2:
@@ -21,10 +23,16 @@ if __name__ == "__main__":
                 file_name = file_content.name
                 if ".java" in file_name:
                     decoded_file_content_lines = file_content.decoded_content.decode("utf-8").split("\n")
-                    print(f"\n{path}")
-                    analyze_file(decoded_file_content_lines)
+                    analyze_file(decoded_file_content_lines,path,json_dict)
                 elif build_lib == None:
                     build_lib=get_build_lib(file_name)
+                    data = {"file_path": path,
+                        "build_lib": build_lib}
+                    json_dict["config_file"]=data
+
+        f = open("test_locations.json","w")
+        json.dump(json_dict,f)
+        f.close()
 
                     
     else:
--- test-scraper-git/scraper_git.py
+++ test-scraper-git/scraper_git.py
@@ -1,6 +1,7 @@
 from inspect import currentframe
 import re
 from pathlib import Path
+import json
 
 IMPORTS = ['import org.junit.*']
 ANNOTATIONS = ['@Test', '@RunWith']
@@ -12,7 +13,7 @@ def find_imports(line, index):
         found = re.search(regex, line)
 
         if found:
-            imports_found.append("{} - line {}".format(line.strip(), index))
+            imports_found.append("{}".format(line.strip()))
     
     return imports_found
 
@@ -34,10 +35,10 @@ def gather_info(annotations_found, imports_found, line, index):
     import_found = find_imports(line, index)
 
     if annotation_found:
-        annotations_found.append(annotation_found)
+        annotations_found.append(annotation_found[0])
 
     if import_found:
-        imports_found.append(import_found)
+        imports_found.append(import_found[0])
 
     return annotations_found, imports_found
 
@@ -46,20 +47,28 @@ def display_results(results):
         print("{} FOUND: {}".format(key, value))
 
 
-def analyze_file(lines):
-
+def analyze_file(lines,file_path,json_list):
     annotations_found = []
     imports_found = []
 
     for i in range(len(lines)):
         annotations_found, imports_found = gather_info(annotations_found, imports_found, lines[i], i)
+    
+    if len(annotations_found) > 0 or len(imports_found) > 0:
+        index = len(json_list.keys())
+        key = "file_" + str(index)
+        data = {"file_path": file_path,
+                "annotations": annotations_found,
+                "number_of_annotations": len(annotations_found),
+                "imports": imports_found,
+                "number_of_imports": len(imports_found)}
+        json_list[key]=data
 
-    display_results({
-        "ANNOTATIONS": [a[0] for a in annotations_found],
-        "IMPORTS": [i[0] for i in imports_found]
-    })
+    # display_results({
+    #     "ANNOTATIONS": [a[0] for a in annotations_found],
+    #     "IMPORTS": [i[0] for i in imports_found]
+    # })
 
-    print("----------------------------")
 
 def get_build_lib(file_name):
     if 'pom.xml' == file_name:
