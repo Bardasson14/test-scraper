20cc7d464963750e41601963a6b223ab2d05d328
Vitor Bardasson <vbardasson@id.uff.br>
2023-08-09 12:54:36 -0300

    Adicionando output ao .gitignore

--- main.py
+++ main.py
@@ -11,38 +11,55 @@ TEST_COMMANDS = {
     'gradle': './gradlew test'
 }
 
+
+def analyze_commit(project_name, commit_hash):
+    # TODO: split into functions
+    output_dir = f"output/{project_name}/{commit_hash}"
+    full_dir = PROJECTS_DIR + project_name
+
+    found_tests = {}
+    found_methods = {}
+
+    print(full_dir)
+    subprocess.run(f"git reset --hard {commit_hash}", shell=True, cwd=full_dir)
+    subprocess.run(f"git rev-parse HEAD {commit_hash}", shell=True, cwd=full_dir)
+    analyze_test_directories(full_dir, found_tests)
+    analyze_class_directories(full_dir, found_methods)
+
+    shutil.rmtree(output_dir, ignore_errors=True)
+    os.makedirs(output_dir, exist_ok=True)
+
+    f1 = open(f"{output_dir}/test_locations.json", 'w')
+    json.dump(found_tests, f1)
+    f1.close()
+
+    f2 = open(f"{outp ut_dir}/method_locations.json", 'w')
+    json.dump(found_methods, f2)
+    f2.close()
+    
+    total_tests = 0
+    total_methods = 0
+
+    for test_class in found_tests.keys():
+        total_tests += len(found_tests[test_class]['ANNOTATIONS'])
+
+    for prod_class in found_methods.keys():
+        total_methods += len(found_methods[prod_class]['METHODS'])
+
+    print(f"{project_name} [{commit_hash}]")
+    print("TESTES: ", total_tests)
+    print("MÉTODOS: ", total_methods)
+
+
 if __name__ == "__main__":
     for project_name in os.listdir(PROJECTS_DIR):
-        print(project_name)
         full_dir = PROJECTS_DIR + project_name
-        found_tests = {}
-        found_methods = {}
+        subprocess.run(f"git fetch", shell=True, cwd=full_dir)
+        commit_hash = subprocess.run('git rev-parse --verify HEAD', capture_output=True, shell=True, cwd=full_dir).stdout.decode("utf-8")
+        commit_list = subprocess.run('git rev-list master --first-parent', capture_output=True, shell=True, cwd=full_dir).stdout.decode("utf-8").split()
 
-        analyze_test_directories(full_dir, found_tests)
-        analyze_class_directories(full_dir, found_methods)
+        analyze_commit(project_name, commit_hash)
 
-        commit_hash = subprocess.run('git rev-parse --verify HEAD', capture_output=True, shell=True).stdout.decode("utf-8")
-        output_dir = f"output/{project_name}/{commit_hash}"
-        shutil.rmtree(output_dir, ignore_errors=True)
-        os.makedirs(output_dir, exist_ok=True)
-
-        f1 = open(f"{output_dir}/test_locations.json", 'w')
-        json.dump(found_tests, f1)
-        f1.close()
-
-        f2 = open(f"{output_dir}/method_locations.json", 'w')
-        json.dump(found_methods, f2)
-        f2.close()
-        
-        total_tests = 0
-        total_methods = 0
-
-        for test_class in found_tests.keys():
-            total_tests += len(found_tests[test_class]['ANNOTATIONS'])
-
-        for prod_class in found_methods.keys():
-            total_methods += len(found_methods[prod_class]['METHODS'])
-
-        print("PROJETO: ", project_name)
-        print("TESTES: ", total_tests)
-        print("MÉTODOS: ", total_methods)
+        for commit_full_hash in commit_list[:10]:
+            if commit_full_hash != commit_hash:
+                analyze_commit(project_name, commit_full_hash)
